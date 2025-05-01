dabc39b7ba28389865835c86069c399fb6653f13
jewelrana3 <juyelrana7752@gmail.com>
2025-05-01 16:08:41 +0600

    server link add

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://10.0.70.188:5003/api/v1',
+        baseUrl: 'http://178.16.138.188:5003/api/v1',
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('accessToken');
             if (token) {
@@ -16,4 +16,4 @@ export const api = createApi({
     endpoints: () => ({}),
 });
 
-export const imgUrl = 'http://10.0.70.188:5003';
+export const imgUrl = 'http://178.16.138.188:5003';
