a00637a645e4511119bff63bea08f5d92acf3d4d
jewelrana3 <juyelrana7752@gmail.com>
2025-05-27 15:41:22 +0600

    new url add baseRoute

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react()],
     server: {
         // host: '178.16.138.188',
-        // port: 5000,
+        host: '178.16.138.188',
+        // port: 5173,
     },
 });
