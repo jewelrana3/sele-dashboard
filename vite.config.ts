1b1c47f218d166c0169089b275d64c35267b69bc
jewelrana3 <juyelrana7752@gmail.com>
2025-06-27 18:39:00 +0600

    add again set host url

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-
     server: {
-        host: 'https://api.selerental.com',
+        host: '69.62.67.125',
     },
 });
